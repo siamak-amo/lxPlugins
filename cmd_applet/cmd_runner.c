e8db8647f6b04bcebb1c05a83480a14f72bba03b
siamak a.m.o <edu.siamak@gmail.com>
2026-07-24 21:57:08 +0330

    Rename & Cleanup

--- cmd_applet/cmd_runner.c
+++ cmd_applet/cmd_runner.c
@@ -22,13 +22,17 @@
 #include <string.h>
 #include <pthread.h>
 
-#include <lxpanel/plugin.h>
 #include <glib/gi18n.h>
+#include <lxpanel/plugin.h>
 
 #include "exec.h"
 
 #define MIN_OUTPUT_LENGTH (3) // utf-8 characters
 #define MAX_OUTPUT_LENGTH DEFAULT_BUF_CAP
+
+#ifndef DEFAULT_OUTPUT_LENGTH
+#define DEFAULT_OUTPUT_LENGTH (42)
+#endif
 
 #define MIN_UPDATE_INTERVAL (1) // 1(s)
 #define MAX_UPDATE_INTERVAL (60 * 60) // 1(H)
@@ -38,9 +42,6 @@
 #endif
 #ifndef DEFAULT_UPDATE_INTERVAL
 #define DEFAULT_UPDATE_INTERVAL (1)
-#endif
-#ifndef DEFAULT_OUTPUT_LENGTH
-#define DEFAULT_OUTPUT_LENGTH (42)
 #endif
 
 #ifndef DEFAULT_CMD
@@ -53,7 +54,7 @@
 #define EXEC_PLACEHOLDER " <--> "
 #endif
 
-typedef struct Cmdout
+typedef struct /* Main applet descriptor */
 {
   int bold_text;
   int center_text;
@@ -83,15 +84,15 @@ typedef struct Cmdout
   /* command runner. */
   CMD exec_cmd;
   int exec_ret;
-} Cmdout;
+} Cmdapp;
 
-static gboolean cmdo_update (Cmdout *cmdo);
-static void cmdo_destructor (gpointer user_data);
-static GtkWidget *cmdo_constructor (LXPanel *panel, config_setting_t *settings);
-static gboolean cmdo_apply_configuration (gpointer user_data);
-static void cmdo_on_panel_reconfigured (LXPanel *panel, GtkWidget *p);
-static GtkWidget *cmdo_configure (LXPanel *panel, GtkWidget *p);
-static void load_from_cfg (Cmdout *cmdo);
+static gboolean cmdp_update (Cmdapp *cmdp);
+static void cmdp_destructor (gpointer user_data);
+static GtkWidget *cmdp_constructor (LXPanel *panel, config_setting_t *settings);
+static gboolean cmdp_apply_configuration (gpointer user_data);
+static void cmdp_on_panel_reconfigured (LXPanel *panel, GtkWidget *p);
+static GtkWidget *cmdp_configure (LXPanel *panel, GtkWidget *p);
+static void load_from_cfg (Cmdapp *cmdp);
 
 static inline void *
 g_restrdup (void *restrict gptr, const char *restrict src)
@@ -101,87 +102,87 @@ g_restrdup (void *restrict gptr, const char *restrict src)
 }
 
 static void
-cmdo_destructor (gpointer user_data)
+cmdp_destructor (gpointer user_data)
 {
-  Cmdout *cmdo = user_data;
-  if (cmdo->timer != 0)
-    g_source_remove (cmdo->timer);
+  Cmdapp *cmdp = user_data;
+  if (cmdp->timer != 0)
+    g_source_remove (cmdp->timer);
 
   /* Terminate & Cleanup thread stuff */
-  pthread_cancel (cmdo->runner_thread);
-  pthread_join (cmdo->runner_thread, NULL);
-  pthread_cond_destroy (&cmdo->runner_cond);
-  pthread_mutex_destroy (&cmdo->runner_mutex);
+  pthread_cancel (cmdp->runner_thread);
+  pthread_join (cmdp->runner_thread, NULL);
+  pthread_cond_destroy (&cmdp->runner_cond);
+  pthread_mutex_destroy (&cmdp->runner_mutex);
 
   /* Deallocate all memory. */
-  g_free (cmdo->exec_cmd.args);
-  g_free (cmdo->cmd);
-  g_free (cmdo);
+  g_free (cmdp->exec_cmd.args);
+  g_free (cmdp->cmd);
+  g_free (cmdp);
 }
 
 void *
 runner_th (void *_ptr)
 {
+  Cmdapp *cmdp = (Cmdapp *) _ptr;
   /*
    * Do not call any lxxx_draw_yyy() function here,
    * drawing on the panel using this function (form another thread),
-   * will freeze the panel GUI after some time.
+   * will make the GUI of the panel frozen after some time.
    */
-  Cmdout *cmdo = (Cmdout *) _ptr;
   while (1)
     {
-      /* going to bed until cmdo_update() wakes us up */
-      while (cmdo->runner_state != RUNT_READY)
-        pthread_cond_wait (&cmdo->runner_cond, &cmdo->runner_mutex);
+      /* going to bed until cmdp_update() wakes us up */
+      while (cmdp->runner_state != RUNT_READY)
+        pthread_cond_wait (&cmdp->runner_cond, &cmdp->runner_mutex);
 
-      cmdo->runner_state = RUNT_RUNNING;
-      cmdo->exec_ret = exec_run (&cmdo->exec_cmd); // blocking.
-      cmdo->runner_state = RUNT_DONE;
+      cmdp->runner_state = RUNT_RUNNING;
+      cmdp->exec_ret = exec_run (&cmdp->exec_cmd); // blocking.
+      cmdp->runner_state = RUNT_DONE;
     }
 
   pthread_exit (NULL);
 }
 
 static gboolean
-cmdo_update (Cmdout *cmdo)
+cmdp_update (Cmdapp *cmdp)
 {
   const char *res;
   gchar *res_utf8;
 
   if (g_source_is_destroyed (g_main_current_source ()))
     return FALSE;
-  cmdo->timer = g_timeout_add (cmdo->refresh_interval * 1000,
-                               (GSourceFunc) cmdo_update, (gpointer) cmdo);
+  cmdp->timer = g_timeout_add (cmdp->refresh_interval * 1000,
+                               (GSourceFunc) cmdp_update, (gpointer) cmdp);
 
-  switch (cmdo->runner_state)
+  switch (cmdp->runner_state)
     {
     case RUNT_RUNNING:
     case RUNT_READY: // unreachable.
       break;
 
     case RUNT_DONE:
-      if (0 != cmdo->exec_ret)
-        lxpanel_draw_label_text (cmdo->panel, cmdo->gtext, EXEC_FAILED,
-                                 cmdo->bold_text, 1, TRUE);
+      if (0 != cmdp->exec_ret)
+        lxpanel_draw_label_text (cmdp->panel, cmdp->gtext, EXEC_FAILED,
+                                 cmdp->bold_text, 1, TRUE);
       else /* exec() success */
         {
-          res = exec_tr (&cmdo->exec_cmd, '\n', ' ');
+          res = exec_tr (&cmdp->exec_cmd, '\n', ' ');
           if ((res_utf8 = g_locale_to_utf8 (res, -1, NULL, NULL, NULL)))
             {
-              lxpanel_draw_label_text (cmdo->panel, cmdo->gtext, res_utf8,
-                                       cmdo->bold_text, 1, TRUE);
+              lxpanel_draw_label_text (cmdp->panel, cmdp->gtext, res_utf8,
+                                       cmdp->bold_text, 1, TRUE);
               g_free (res_utf8);
             }
         }
-      cmdo->runner_state = RUNT_READY;
-      pthread_cond_signal (&cmdo->runner_cond);
+      cmdp->runner_state = RUNT_READY;
+      pthread_cond_signal (&cmdp->runner_cond);
       break;
 
     case RUNT_IDLE:
-      lxpanel_draw_label_text (cmdo->panel, cmdo->gtext, EXEC_PLACEHOLDER,
-                               cmdo->bold_text, 1, TRUE);
-      cmdo->runner_state = RUNT_READY;
-      pthread_cond_signal (&cmdo->runner_cond);
+      lxpanel_draw_label_text (cmdp->panel, cmdp->gtext, EXEC_PLACEHOLDER,
+                               cmdp->bold_text, 1, TRUE);
+      cmdp->runner_state = RUNT_READY;
+      pthread_cond_signal (&cmdp->runner_cond);
       break;
     }
 
@@ -189,55 +190,55 @@ cmdo_update (Cmdout *cmdo)
 }
 
 static void
-ensure_cfg (Cmdout *cmdo)
+ensure_cfg (Cmdapp *cmdp)
 {
-  if (! cmdo->cmd || '\0' == cmdo->cmd[0])
+  if (! cmdp->cmd || '\0' == cmdp->cmd[0])
     {
-      cmdo->run_with_shell = true;
-      cmdo->cmd = g_restrdup (cmdo->cmd, DEFAULT_CMD);
+      cmdp->run_with_shell = true;
+      cmdp->cmd = g_restrdup (cmdp->cmd, DEFAULT_CMD);
     }
-  if (0 == cmdo->exec_cmd.timeout_ms)
-    cmdo->exec_cmd.timeout_ms = EXEC_TIMEOUT;
-  if (MIN_OUTPUT_LENGTH >= cmdo->exec_cmd.max_output_len ||
-      MAX_OUTPUT_LENGTH <= cmdo->exec_cmd.max_output_len)
-    cmdo->exec_cmd.max_output_len = DEFAULT_OUTPUT_LENGTH;
-  if (MIN_UPDATE_INTERVAL >= cmdo->refresh_interval)
-    cmdo->refresh_interval = DEFAULT_UPDATE_INTERVAL;
+  if (0 == cmdp->exec_cmd.timeout_ms)
+    cmdp->exec_cmd.timeout_ms = EXEC_TIMEOUT;
+  if (MIN_OUTPUT_LENGTH >= cmdp->exec_cmd.max_output_len ||
+      MAX_OUTPUT_LENGTH <= cmdp->exec_cmd.max_output_len)
+    cmdp->exec_cmd.max_output_len = DEFAULT_OUTPUT_LENGTH;
+  if (MIN_UPDATE_INTERVAL >= cmdp->refresh_interval)
+    cmdp->refresh_interval = DEFAULT_UPDATE_INTERVAL;
 
   /* our execvp() call will have either 2 or 4 args. */
-  if (! cmdo->exec_cmd.args)
-    cmdo->exec_cmd.args = g_new0 (const char *, 4);
-  if (cmdo->run_with_shell)
+  if (! cmdp->exec_cmd.args)
+    cmdp->exec_cmd.args = g_new0 (const char *, 4);
+  if (cmdp->run_with_shell)
     { /* 4 args. */
-      cmdo->exec_cmd.execvp_bin = cmdo->shell;
-      cmdo->exec_cmd.args[0] = cmdo->shell;
-      cmdo->exec_cmd.args[1] = "-c";
-      cmdo->exec_cmd.args[2] = cmdo->cmd;
-      cmdo->exec_cmd.args[3] = NULL;
+      cmdp->exec_cmd.execvp_bin = cmdp->shell;
+      cmdp->exec_cmd.args[0] = cmdp->shell;
+      cmdp->exec_cmd.args[1] = "-c";
+      cmdp->exec_cmd.args[2] = cmdp->cmd;
+      cmdp->exec_cmd.args[3] = NULL;
     }
   else /* 2 args. */
     {
-      cmdo->exec_cmd.execvp_bin = cmdo->cmd;
-      cmdo->exec_cmd.args[0] = cmdo->cmd;
-      cmdo->exec_cmd.args[1] = NULL;
+      cmdp->exec_cmd.execvp_bin = cmdp->cmd;
+      cmdp->exec_cmd.args[0] = cmdp->cmd;
+      cmdp->exec_cmd.args[1] = NULL;
     }
 }
 
 /* Plugin constructor */
 static GtkWidget *
-cmdo_constructor (LXPanel *panel, config_setting_t *settings)
+cmdp_constructor (LXPanel *panel, config_setting_t *settings)
 {
   GtkWidget *p;
-  Cmdout * cmdo = g_new0 (Cmdout, 1);
-  cmdo->panel = panel;
-  cmdo->settings = settings;
+  Cmdapp * cmdp = g_new0 (Cmdapp, 1);
+  cmdp->panel = panel;
+  cmdp->settings = settings;
 
-  cmdo->plugin = p = gtk_event_box_new ();
-  lxpanel_plugin_set_data (p, cmdo, cmdo_destructor);
+  cmdp->plugin = p = gtk_event_box_new ();
+  lxpanel_plugin_set_data (p, cmdp, cmdp_destructor);
 
-  load_from_cfg (cmdo);
-  ensure_cfg (cmdo);
-  exec_init (&cmdo->exec_cmd);
+  load_from_cfg (cmdp);
+  ensure_cfg (cmdp);
+  exec_init (&cmdp->exec_cmd);
 
   /* Put the label inside an event box so it can handle clicks
      (do we really need this?) */
@@ -247,106 +248,106 @@ cmdo_constructor (LXPanel *panel, config_setting_t *settings)
     GtkWidget * hbox = gtk_hbox_new (TRUE, 0);
     gtk_container_add (GTK_CONTAINER(p), hbox);
     gtk_widget_show (hbox);
-    cmdo->gtext = gtk_label_new (NULL);
-    gtk_misc_set_alignment (GTK_MISC(cmdo->gtext), 0.5, 0.5);
-    gtk_misc_set_padding (GTK_MISC(cmdo->gtext), 4, 0);
-    gtk_container_add (GTK_CONTAINER(hbox), cmdo->gtext);
+    cmdp->gtext = gtk_label_new (NULL);
+    gtk_misc_set_alignment (GTK_MISC(cmdp->gtext), 0.5, 0.5);
+    gtk_misc_set_padding (GTK_MISC(cmdp->gtext), 4, 0);
+    gtk_container_add (GTK_CONTAINER(hbox), cmdp->gtext);
   }
   gtk_widget_show_all (p);
 
-  cmdo->runner_state = RUNT_IDLE;
-  pthread_cond_init (&cmdo->runner_cond, NULL);
-  pthread_mutex_init(&cmdo->runner_mutex, NULL);
-  pthread_create (&cmdo->runner_thread, NULL, runner_th, cmdo);
+  cmdp->runner_state = RUNT_IDLE;
+  pthread_cond_init (&cmdp->runner_cond, NULL);
+  pthread_mutex_init(&cmdp->runner_mutex, NULL);
+  pthread_create (&cmdp->runner_thread, NULL, runner_th, cmdp);
+  cmdp->timer = g_idle_add ((GSourceFunc) cmdp_update, cmdp);
 
-  cmdo->timer = g_idle_add ((GSourceFunc) cmdo_update, cmdo);
   return p;
 }
 
 static gboolean
-cmdo_apply_configuration (gpointer user_data)
+cmdp_apply_configuration (gpointer user_data)
 {
   GtkWidget * p = user_data;
-  Cmdout *cmdo = lxpanel_plugin_get_data(p);
-  ensure_cfg (cmdo);
+  Cmdapp *cmdp = lxpanel_plugin_get_data(p);
+  ensure_cfg (cmdp);
 
   /* stop the updater now */
-  if (cmdo->timer)
-    g_source_remove (cmdo->timer);
+  if (cmdp->timer)
+    g_source_remove (cmdp->timer);
 
-  if (cmdo->center_text)
-    gtk_label_set_justify (GTK_LABEL(cmdo->gtext), GTK_JUSTIFY_CENTER);
+  if (cmdp->center_text)
+    gtk_label_set_justify (GTK_LABEL(cmdp->gtext), GTK_JUSTIFY_CENTER);
   else
-    gtk_label_set_justify (GTK_LABEL(cmdo->gtext), GTK_JUSTIFY_LEFT);
+    gtk_label_set_justify (GTK_LABEL(cmdp->gtext), GTK_JUSTIFY_LEFT);
 
-  if (cmdo->exec_cmd.buf)
-    cmdo->exec_cmd.buf[0] = '\0';
+  if (cmdp->exec_cmd.buf)
+    cmdp->exec_cmd.buf[0] = '\0';
 
   /* Save configuration */
-  config_group_set_string (cmdo->settings, "Command", cmdo->cmd);
-  config_group_set_int (   cmdo->settings, "BoldFont", cmdo->bold_text);
-  config_group_set_int (   cmdo->settings, "CenterText", cmdo->center_text);
-  config_group_set_int (   cmdo->settings, "WithShell", cmdo->run_with_shell);
-  config_group_set_int (   cmdo->settings, "MaxLength", cmdo->exec_cmd.max_output_len);
-  config_group_set_int (   cmdo->settings, "ShowStderr", cmdo->exec_cmd.redirect_stderr);
-  config_group_set_int (   cmdo->settings, "UpdateInterval", cmdo->refresh_interval);  
+  config_group_set_string (cmdp->settings, "Command", cmdp->cmd);
+  config_group_set_int (   cmdp->settings, "BoldFont", cmdp->bold_text);
+  config_group_set_int (   cmdp->settings, "CenterText", cmdp->center_text);
+  config_group_set_int (   cmdp->settings, "WithShell", cmdp->run_with_shell);
+  config_group_set_int (   cmdp->settings, "MaxLength", cmdp->exec_cmd.max_output_len);
+  config_group_set_int (   cmdp->settings, "ShowStderr", cmdp->exec_cmd.redirect_stderr);
+  config_group_set_int (   cmdp->settings, "UpdateInterval", cmdp->refresh_interval);  
 
   /* spawn the updater again. */
-  pthread_cancel (cmdo->runner_thread);
-  pthread_join (cmdo->runner_thread, NULL);
-  cmdo->runner_state = RUNT_IDLE;
-  pthread_create (&cmdo->runner_thread, NULL, runner_th, cmdo);
-  cmdo->timer = g_idle_add ((GSourceFunc) cmdo_update, cmdo);
+  pthread_cancel (cmdp->runner_thread);
+  pthread_join (cmdp->runner_thread, NULL);
+  cmdp->runner_state = RUNT_IDLE;
+  pthread_create (&cmdp->runner_thread, NULL, runner_th, cmdp);
+  cmdp->timer = g_idle_add ((GSourceFunc) cmdp_update, cmdp);
   return FALSE;
 }
 
 static void
-load_from_cfg (Cmdout *cmdo)
+load_from_cfg (Cmdapp *cmdp)
 {
   int tmp_int;
   const char *tmp_str;
 
-  if (config_setting_lookup_string (cmdo->settings, "Command", &tmp_str))
-    cmdo->cmd = g_restrdup (cmdo->cmd, tmp_str);
-  if (config_setting_lookup_int (   cmdo->settings, "BoldFont", &tmp_int))
-    cmdo->bold_text = (tmp_int != 0);
-  if (config_setting_lookup_int (   cmdo->settings, "CenterText", &tmp_int))
-    cmdo->center_text = (tmp_int != 0);
-  if (config_setting_lookup_int (   cmdo->settings, "WithShell", &tmp_int))
-    cmdo->run_with_shell = (tmp_int != 0);
-  if (config_setting_lookup_int (   cmdo->settings, "MaxLength", &tmp_int))
-    cmdo->exec_cmd.max_output_len = (tmp_int >= 0) ? tmp_int : DEFAULT_OUTPUT_LENGTH;
-  if (config_setting_lookup_int (   cmdo->settings, "ShowStderr", &tmp_int))
-    cmdo->exec_cmd.redirect_stderr = (tmp_int != 0);
-  if (config_setting_lookup_int (   cmdo->settings, "UpdateInterval", &tmp_int))
-    cmdo->refresh_interval = (tmp_int > 0) ? tmp_int : DEFAULT_UPDATE_INTERVAL;
+  if (config_setting_lookup_string (cmdp->settings, "Command", &tmp_str))
+    cmdp->cmd = g_restrdup (cmdp->cmd, tmp_str);
+  if (config_setting_lookup_int (   cmdp->settings, "BoldFont", &tmp_int))
+    cmdp->bold_text = (tmp_int != 0);
+  if (config_setting_lookup_int (   cmdp->settings, "CenterText", &tmp_int))
+    cmdp->center_text = (tmp_int != 0);
+  if (config_setting_lookup_int (   cmdp->settings, "WithShell", &tmp_int))
+    cmdp->run_with_shell = (tmp_int != 0);
+  if (config_setting_lookup_int (   cmdp->settings, "MaxLength", &tmp_int))
+    cmdp->exec_cmd.max_output_len = (tmp_int >= 0) ? tmp_int : DEFAULT_OUTPUT_LENGTH;
+  if (config_setting_lookup_int (   cmdp->settings, "ShowStderr", &tmp_int))
+    cmdp->exec_cmd.redirect_stderr = (tmp_int != 0);
+  if (config_setting_lookup_int (   cmdp->settings, "UpdateInterval", &tmp_int))
+    cmdp->refresh_interval = (tmp_int > 0) ? tmp_int : DEFAULT_UPDATE_INTERVAL;
 }
 
 static GtkWidget *
-cmdo_configure (LXPanel *panel, GtkWidget *p)
+cmdp_configure (LXPanel *panel, GtkWidget *p)
 {
-  Cmdout *cmdo = lxpanel_plugin_get_data (p);
+  Cmdapp *cmdp = lxpanel_plugin_get_data (p);
   GtkWidget *res = lxpanel_generic_config_dlg (_("Command output applet"), panel,
-                                               cmdo_apply_configuration, p,
-        _("Command"), &cmdo->cmd, CONF_TYPE_STR,
-        _("Bold font"), &cmdo->bold_text, CONF_TYPE_BOOL,
-        _("Center text"), &cmdo->center_text, CONF_TYPE_BOOL,
-        _("Include stderr"), &cmdo->exec_cmd.redirect_stderr, CONF_TYPE_BOOL,
-        _("Run with shell"), &cmdo->run_with_shell, CONF_TYPE_BOOL,
+                                               cmdp_apply_configuration, p,
+        _("Command"), &cmdp->cmd, CONF_TYPE_STR,
+        _("Bold font"), &cmdp->bold_text, CONF_TYPE_BOOL,
+        _("Center text"), &cmdp->center_text, CONF_TYPE_BOOL,
+        _("Include stderr"), &cmdp->exec_cmd.redirect_stderr, CONF_TYPE_BOOL,
+        _("Run with shell"), &cmdp->run_with_shell, CONF_TYPE_BOOL,
             _("run the given command with the default system shell;"
               " or as a system command;"), NULL, CONF_TYPE_TRIM,
-        _("Maximum length"), &cmdo->exec_cmd.max_output_len, CONF_TYPE_INT,
+        _("Maximum length"), &cmdp->exec_cmd.max_output_len, CONF_TYPE_INT,
             _("set it zero to get the maximum allowed length"), NULL, CONF_TYPE_TRIM,
-        _("Update interval (s)"), &cmdo->refresh_interval, CONF_TYPE_INT,
+        _("Update interval (s)"), &cmdp->refresh_interval, CONF_TYPE_INT,
         NULL);
   return res;
 }
 
 static void
-cmdo_on_panel_reconfigured (LXPanel *panel, GtkWidget *p)
+cmdp_on_panel_reconfigured (LXPanel *panel, GtkWidget *p)
 {
   (void) panel;
-  cmdo_apply_configuration(p);
+  cmdp_apply_configuration(p);
 }
 
 /* Plugin descriptor */
@@ -357,8 +358,8 @@ LXPanelPluginInit fm_module_init_lxpanel_gtk =
     .description = "Display output of system commands",
     .one_per_system = 0, /* Allow multiple instances */
 
-    .new_instance        = cmdo_constructor,
-    .config              = cmdo_configure,
-    .reconfigure         = cmdo_on_panel_reconfigured,
+    .new_instance        = cmdp_constructor,
+    .config              = cmdp_configure,
+    .reconfigure         = cmdp_on_panel_reconfigured,
   };
 FM_DEFINE_MODULE (lxpanel_gtk, cmd_runner);
