3925bebf37502d86901d0c1bdb19aad125ff0f17
siamak a.m.o <edu.siamak@gmail.com>
2026-09-23 12:27:52 +0330

    Cleanup

--- cmd_applet/COMPILEME.c
+++ cmd_applet/COMPILEME.c
@@ -6,8 +6,7 @@
     cc -Wall -Wextra -Werror \
        `pkg-config --cflags gtk+-2.0 lxpanel` \
        -shared -fPIC COMPILEME.c -o cmd_runner.so \
-       `pkg-config --libs lxpanel` \
-       -Wl,-rpath,/usr/lib/x86_64-linux-gnu/lxpanel
+       `pkg-config --libs lxpanel` -Wl,-rpath,/usr/lib/x86_64-linux-gnu/lxpanel
  */
 #include "cmd_runner.c"
 #include "exec.c"
--- jalalidate_applet/COMPILEME.c
+++ jalalidate_applet/COMPILEME.c
@@ -4,11 +4,9 @@
   Compile Me file.
   Compilation:
     cc -Wall -Wextra -Werror \
-       `pkg-config --cflags gtk+-2.0 lxpanel` \
-       -I../lib/libjalali/ \
+       `pkg-config --cflags gtk+-2.0 lxpanel`  -I../lib/libjalali/ \
        -shared -fPIC COMPILEME.c -o jdclock.so \
-       `pkg-config --libs lxpanel` \
-       -Wl,-rpath,/usr/lib/x86_64-linux-gnu/lxpanel
+       `pkg-config --libs lxpanel`  -Wl,-rpath,/usr/lib/x86_64-linux-gnu/lxpanel
  */
 #include "jdclock.c"
 
